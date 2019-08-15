01645e3decb0cf2212f1b9ab1fa1fd1d29c368cd
Carolina <carolinaevevarela@gmail.com>
2019-08-14 21:54:27 -0400

    Adding Role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  enum role: [:bookstore, :editor, :admin]
+    after_initialize :set_default_role, :if => :new_record?
+
+    def set_default_role
+      self.role ||= :admin
+    end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
