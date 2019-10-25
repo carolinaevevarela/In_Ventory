96786e2172ff6e17fdec83c3eae65c764753f289
Carolina <carolinaevevarela@gmail.com>
2019-10-24 22:36:15 -0300

    Texto en español

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
-  before_action :set_categories
+  before_action :set_categories, :set_locale
 
   private
 
   def set_categories
    @categoriesmenu = Category.all
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    extract_locale_from_accept_language_header ||
+    I18n.default_locale
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    locale if ['es', 'en'].include? locale.to_s
   end
 end
